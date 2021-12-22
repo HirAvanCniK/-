682db5f9f17748fdc5b6ba638960af888b660109
HirAvanCniK <vanniandrea244466666@gmail.com>
2021-12-22 16:43:51 +0100

    THE SUPREME BOT

--- commands/🎮 Game Commands/tictactoe.js
+++ commands/🎮 Game Commands/tictactoe.js
@@ -1,17 +1,9 @@
-const { tictactoe } = require('reconlx')
-
 module.exports = {
     name: 'tris',
     category: "🎮 Game Commands",
     description: "play a famous game tictactoe",
-    usage: "tris <user>",
+    usage: "tris [user]",
     run : async(client, message, args) => {
-        const member = message.mentions.members.first() 
-            if(!member)  return  message.channel.send('Please specify a member')
-        
-        new tictactoe({
-            player_two: member, 
-            message: message
-        })
+        console.log("Comando tris avvenuto con successo.");
     }
 }
